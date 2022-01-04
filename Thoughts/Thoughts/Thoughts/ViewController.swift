208fdde9e32ba7e1b7211093cc9c827da8a37307
timothylidede <lidedetimothy@gmail.com>
2022-01-04 03:44:56 +0300

    creation of project

---
+++ Thoughts/Thoughts/Thoughts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by George Okello on 04/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
