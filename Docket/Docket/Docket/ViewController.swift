184ab58570ff9ecb0bc2e84cb05bdb6889e9cd56
timothylidede <lidedetimothy@gmail.com>
2022-01-08 00:57:55 +0300

    creation of project

---
+++ Docket/Docket/Docket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Docket
+//
+//  Created by George Okello on 08/01/2022.
+//  Copyright © 2022 Tim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Docket/Docket/DocketTests/DocketTests.swift
@@ -0,0 +1,34 @@
+//
+//  DocketTests.swift
+//  DocketTests
+//
+//  Created by George Okello on 08/01/2022.
+//  Copyright © 2022 Tim. All rights reserved.
+//
+
+import XCTest
+@testable import Docket
+
+class DocketTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
