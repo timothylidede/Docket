d4536a428fec2c73e635f6bac2399ed3da090e8a
timothylidede <lidedetimothy@gmail.com>
2022-01-09 03:11:35 +0300

    dequeing cell

--- Tasks/Tasks/Tasks/ViewController.swift
+++ Tasks/Tasks/Tasks/ViewController.swift
@@ -25,13 +25,21 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true <#T##Bool#>)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
     }
 }
 
