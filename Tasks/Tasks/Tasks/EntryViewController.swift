86439921c3437f508753401188c2cf8487da4036
timothylidede <lidedetimothy@gmail.com>
2022-01-09 03:26:01 +0300

    deleting comments

--- Tasks/Tasks/Tasks/EntryViewController.swift
+++ Tasks/Tasks/Tasks/EntryViewController.swift
@@ -1,11 +1,3 @@
-//
-//  EntryViewController.swift
-//  Tasks
-//
-//  Created by George Okello on 09/01/2022.
-//  Copyright © 2022 Tim. All rights reserved.
-//
-
 import UIKit
 
 class EntryViewController: UIViewController {
@@ -15,16 +7,5 @@ class EntryViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
