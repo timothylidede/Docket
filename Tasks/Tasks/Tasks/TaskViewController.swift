ed2b68af4538b768bc6d1de42ba7eec3eab08872
timothylidede <lidedetimothy@gmail.com>
2022-01-09 03:27:33 +0300

    deleting comments

--- Tasks/Tasks/Tasks/TaskViewController.swift
+++ Tasks/Tasks/Tasks/TaskViewController.swift
@@ -1,11 +1,3 @@
-//
-//  TaskViewController.swift
-//  Tasks
-//
-//  Created by George Okello on 09/01/2022.
-//  Copyright © 2022 Tim. All rights reserved.
-//
-
 import UIKit
 
 class TaskViewController: UIViewController {
@@ -15,16 +7,5 @@ class TaskViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
