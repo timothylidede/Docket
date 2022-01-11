0ea830e0f45fce98bbd1df1890da5b5d71f51023
timothylidede <lidedetimothy@gmail.com>
2022-01-12 01:37:39 +0300

    adding to task array

--- Tasks/Tasks/Tasks/ViewController.swift
+++ Tasks/Tasks/Tasks/ViewController.swift
@@ -39,8 +39,11 @@ class ViewController: UIViewController {
             return
         }
         
-        for x in 0...count{
+        for x in 0..<count{
             
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String{
+                tasks.append(task)
+            }
         }
         
     }
