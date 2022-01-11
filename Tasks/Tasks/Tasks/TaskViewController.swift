79716f40fde71ffe9b1bfac43a58a909e08e655f
timothylidede <lidedetimothy@gmail.com>
2022-01-12 01:51:48 +0300

    setting var task to label

--- Tasks/Tasks/Tasks/TaskViewController.swift
+++ Tasks/Tasks/Tasks/TaskViewController.swift
@@ -4,12 +4,13 @@ class TaskViewController: UIViewController {
     
     @IBOutlet var label: UILabel!
     
-    
+    var task: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        label.text = task
     }
 
 }
