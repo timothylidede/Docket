dd9e6c239eeffeba9cdbcbf8bf70dfe80f9bc604
timothylidede <lidedetimothy@gmail.com>
2022-01-13 00:16:27 +0300

    altering deletetask fun content

--- Tasks/Tasks/Tasks/TaskViewController.swift
+++ Tasks/Tasks/Tasks/TaskViewController.swift
@@ -17,11 +17,13 @@ class TaskViewController: UIViewController {
     
     @objc func deleteTask(){
         
+        /*
         let newCount = count - 1
         
         UserDefaults().setValue(newCount, forKey: "count")
         UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
-
+ 
+        */
 
     }
 
